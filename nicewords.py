a1e3f7bf63a2b34642667653b8026e2ca8ade5d5
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 14:34:01 +0200

    fetch page

--- nicewords.py
+++ nicewords.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append("/home/rakaut/containers/webbuilder/modules")
 
-
 import dbConn as db
+import urllib.request
 
 
-print("Test")
+page = urllib.request.urlopen('https://www.affirmations.dev/')
+print(page.read())
