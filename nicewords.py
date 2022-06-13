5237582531d2aca0ae8b19099a782af1ef31d694
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 14:40:11 +0200

    page.read()

--- nicewords.py
+++ nicewords.py
@@ -1,9 +1,14 @@
 import sys
 sys.path.append("/home/rakaut/containers/webbuilder/modules")
-
+import json
 import dbConn as db
 import urllib.request
 
 
 page = urllib.request.urlopen('https://www.affirmations.dev/')
 print(page.read())
+
+y = json.dumps(page.read())
+ 
+print(y)
+print(type(y))
