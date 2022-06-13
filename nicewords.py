c674c616b831c52e247f9755f3053f192dbb32a5
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 15:17:47 +0200

    store

--- nicewords.py
+++ nicewords.py
@@ -13,7 +13,15 @@ text = btext.decode('utf-8')
 text=text.replace("{","").replace("b'","").replace("}","").replace('"','')
 print(str(text))
 sp_Text=text.split(":")
-strQ=f"insert into nicewords(type,line)values('{sp_Text[0]}','{sp_Text[1]}')"
-print(strQ)
+strQ=f"select count(*) from  nicewords where type='{sp_Text[0]}' and line)values='{sp_Text[1]}'"
+strCount=db.returnFetchall(strQ)[0]
+print("strCount: {strCount}")
+if strCount == "0":
+    print("Entry not found, storing")
+    strQ=f"insert into nicewords(type,line)values('{sp_Text[0]}','{sp_Text[1]}')"
+    db.NoReturnQuery(strQ)
+else:
+    print(f"{sp_Text[1]} already stored, skipping")
+
 #decodedstring=base64.b64decode(text).decode('utf-8')
 #print(decodedstring) 
