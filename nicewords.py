1d3ab0f3d0cf98ea2ecbb9803af20f7e7ec4f661
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 16:09:04 +0200

    store

--- nicewords.py
+++ nicewords.py
@@ -15,13 +15,13 @@ print(str(text))
 sp_Text=text.split(":")
 #strQ=f"select count(*) from  nicewords where type='%s' and line='%s'"
 strQ=f"select count(*) from  nicewords where type='{sp_Text[0]}' and line='{sp_Text[1]}'"
-print(strQ)
 
 strCount=str(db.returnFetchall(strQ)[0]).replace("(","").replace(")","").split(",")[1]
 print(f"strCount: {strCount}")
 if strCount == "":
     print("Entry not found, storing")
     strQ=f"insert into nicewords(type,line)values('{sp_Text[0]}','{sp_Text[1]}')"
+    print(strQ)
     db.NoReturnQuery(strQ)
 else:
     print(f"{sp_Text[1]} already stored, skipping")
