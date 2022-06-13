9496776d3082953c08c44bbc72473aa9adb96f8b
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 15:35:49 +0200

    store

--- nicewords.py
+++ nicewords.py
@@ -17,7 +17,7 @@ sp_Text=text.split(":")
 strQ=f"select count(*) from  nicewords where type='{sp_Text[0]}' and line='{sp_Text[1]}'"
 print(strQ)
 
-strCount=db.returnFetchall(strQ)[0]
+strCount=db.returnFetchall(strQ)[1]
 print(f"strCount: {strCount}")
 if strCount == "0":
     print("Entry not found, storing")
