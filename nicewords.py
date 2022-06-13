cf9a4c0a0eca7f290b3921f9dc8b5a8ee1920958
Gabberted <43960501+Gabberted@users.noreply.github.com>
2022-06-13 15:09:35 +0200

    text

--- nicewords.py
+++ nicewords.py
@@ -12,6 +12,8 @@ btext=page.read()
 text = btext.decode('utf-8')
 text=text.replace("{","").replace("b'","").replace("}","")
 print(str(text))
-
+sp_Text=text.split(":")
+strQ=f"insert into nicewords(type,line)values('{sp_Text[0]}','{sp_Text[1]}')"
+print(strQ)
 #decodedstring=base64.b64decode(text).decode('utf-8')
 #print(decodedstring) 
